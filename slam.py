c8342837203d76fa5d63bf382ff913a2783c4fc7
mtszkw <mtszkw@protonmail.com>
2019-07-12 10:50:33 +0200

    Add Essential matrix calculation and feature tracking

--- slam.py
+++ slam.py
@@ -1,12 +1,14 @@
 import cv2
 import os
 import time
+import numpy as np
 
 class FrameReader:
     def __init__(self, datasetPath):
-        self._datasetPath = datasetPath
+        __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+        self._datasetPath = os.path.join(__location__, datasetPath)
         self._numFrames = len([x for x in os.listdir(datasetPath) if x.endswith('.jpg')])
-        print("Initializing FrameReader. Found {} images in dataset {}.".format(self._numFrames, self._datasetPath))
+        print("Found {} images in {}".format(self._numFrames, self._datasetPath))
 
     def readFrame(self, index=0):
         if index >= self._numFrames:
@@ -22,34 +24,43 @@ class FrameReader:
 
 
 if __name__ == "__main__":
-    __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
-
-    frameReader = FrameReader(os.path.join(__location__, "videos/sequence_11/images/"))
+    frameReader = FrameReader("videos/sequence_11/images/")
 
     # If there are less than two frames in directory, throw an exception and quit.
     if frameReader.getFramesCount() < 2:
         raise Exception("Not enough images ({}) found, aborting.".format(frameReader.getFramesCount()))
 
     # Read first two frames
-    prevFrame = frameReader.readFrame(0)
-    currFrame = frameReader.readFrame(1)
+    prevFrame = cv2.cvtColor(frameReader.readFrame(0), cv2.COLOR_RGB2GRAY)
+    currFrame = cv2.cvtColor(frameReader.readFrame(1), cv2.COLOR_RGB2GRAY)
     
-    # Initialize FAST feature detector with non max. suppresion and threshold,
-    # Then extract features from 1st and 2nd frame.
+    # Feature detection on the 1st frame
     fast = cv2.FastFeatureDetector_create(threshold=50, nonmaxSuppression=True)
-    prevKeypts = fast.detect(prevFrame, None)
-    currKeypts = fast.detect(currFrame, None)
+    prevPts = cv2.KeyPoint_convert(fast.detect(prevFrame))
+    print("Detected {} features in the first frame".format(len(prevPts)))
 
-    # Find the essential matrix (focal and pp were taken from camera.txt file)
-    focalLength = 1.0
-    principalPoint = (0.493140000000000	0.499021000000000)
-    E = findEssentialMat(prevKeypts, currKeypts, focalLength, principalPoint, RANSAC, 0.99, 1.0) #, mask?)
-    # recoverPose(E, currPoints, prevPoints, R, t, focal, pp, mask?)
+    # Feature tracking on the 2nd frame
+    currPts, status, _ = cv2.calcOpticalFlowPyrLK(prevFrame, currFrame, prevPts, None)
+    print ("Tracked features in the second frame, invalid features: {}".format(np.count_nonzero(status == 0)))
 
-    # for frameIdx in range(frameReader.getFramesCount()):
+    # Filter out keypoints for which status is zero or coordinates are outside the image
+    # ...
+
+    # # Find the essential matrix (focal and p.p. were taken from camera.txt file)
+    fx = 0.349153000000000
+    fy = 0.436593000000000
+    cx = 0.493140000000000
+    cy = 0.499021000000000
+    focal = (fx + fy) / 2
+    pp = (cx, cy)
+    E, mask = cv2.findEssentialMat(prevPts, currPts, focal, pp, cv2.RANSAC, 0.99, 1.0) #, mask?)
+    print("Essential matrix {} calculated:\n{}".format(E.shape, E))
+    # R, t = cv2.recoverPose(E, currPts, prevPts, focal, pp, mask)
+
+    # for frameIdx in range(2, frameReader.getFramesCount()):
     #     frame = frameReader.readFrame(frameIdx)
 
-    #     keypts = fast.detect(frame, None)
+    #     # keypts = fast.detect(frame, None)
     #     frameKeypts = cv2.drawKeypoints(frame, keypts, None, color=(255,0,0))
     #     cv2.imshow("Frame with keypoints", frameKeypts)
 
