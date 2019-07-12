dbdcd433260dd01bdf6d062b9bbd3dc786e1f27b
mtszkw <mtszkw@protonmail.com>
2019-07-12 08:02:41 +0200

    Add camera.txt and first four frames

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 #### Datasets
 - [KITTI Visual Odometry Dataset](http://www.cvlibs.net/datasets/kitti/eval_odometry.php)
 - [Monocular Visual Odometry Dataset (TUM Department of Informatics)](https://vision.in.tum.de/data/datasets/mono-dataset)
+
+#### Understanding the camera.txt format
+As written [here](https://github.com/JakobEngel/dso#31-dataset-format), values in camera.txt file, in case of FOV camera model, stand for:
+```
+FOV fx fy cx cy omega
+in_width in_height
+"crop" / "full" / "fx fy cx cy 0"
+out_width out_height
+```
--- slam.py
+++ slam.py
@@ -2,27 +2,58 @@ import cv2
 import os
 import time
 
+class FrameReader:
+    def __init__(self, datasetPath):
+        self._datasetPath = datasetPath
+        self._numFrames = len([x for x in os.listdir(datasetPath) if x.endswith('.jpg')])
+        print("Initializing FrameReader. Found {} images in dataset {}.".format(self._numFrames, self._datasetPath))
+
+    def readFrame(self, index=0):
+        if index >= self._numFrames:
+            raise Exception("Cannot read frame number {} from {}".format(index, self._datasetPath))
+
+        return cv2.imread(os.path.join(self._datasetPath, "{:05d}.jpg".format(index)))
+
+    def getFramesCount(self):
+        return self._numFrames
+
+    def getDatasetPath(self):
+        return self._datasetPath
+
+
 if __name__ == "__main__":
     __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
-    datasetPath = os.path.join(__location__, "videos/sequence_11/images/")
-    numFrames = len([x for x in os.listdir(datasetPath) if x.endswith('.jpg')])
-    print("Found {} images in dataset {}".format(numFrames, datasetPath))
+    frameReader = FrameReader(os.path.join(__location__, "videos/sequence_11/images/"))
 
-    if numFrames < 2:
-        raise Exception("Not enough images ({}) found in {}, aborting.".format(numFrames, datasetPath))
+    # If there are less than two frames in directory, throw an exception and quit.
+    if frameReader.getFramesCount() < 2:
+        raise Exception("Not enough images ({}) found, aborting.".format(frameReader.getFramesCount()))
+
+    # Read first two frames
+    prevFrame = frameReader.readFrame(0)
+    currFrame = frameReader.readFrame(1)
     
+    # Initialize FAST feature detector with non max. suppresion and threshold,
+    # Then extract features from 1st and 2nd frame.
     fast = cv2.FastFeatureDetector_create(threshold=50, nonmaxSuppression=True)
+    prevKeypts = fast.detect(prevFrame, None)
+    currKeypts = fast.detect(currFrame, None)
 
-    for frameIdx in range(numFrames):
-        frame = cv2.imread(os.path.join(datasetPath, "{:05d}.jpg".format(frameIdx)))
-        # cv2.imshow("Frame", frame)
+    # Find the essential matrix (focal and pp were taken from camera.txt file)
+    focalLength = 1.0
+    principalPoint = (0.493140000000000	0.499021000000000)
+    E = findEssentialMat(prevKeypts, currKeypts, focalLength, principalPoint, RANSAC, 0.99, 1.0) #, mask?)
+    # recoverPose(E, currPoints, prevPoints, R, t, focal, pp, mask?)
 
-        keypts = fast.detect(frame, None)
-        frameKeypts = cv2.drawKeypoints(frame, keypts, None, color=(255,0,0))
-        cv2.imshow("Frame with keypoints", frameKeypts)
+    # for frameIdx in range(frameReader.getFramesCount()):
+    #     frame = frameReader.readFrame(frameIdx)
 
-        if cv2.waitKey(0) == ord('q'):
-            break
+    #     keypts = fast.detect(frame, None)
+    #     frameKeypts = cv2.drawKeypoints(frame, keypts, None, color=(255,0,0))
+    #     cv2.imshow("Frame with keypoints", frameKeypts)
 
-    cv2.destroyAllWindows()
+    #     if cv2.waitKey(1) == ord('q'):
+    #         break
+
+    # cv2.destroyAllWindows()
