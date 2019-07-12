fe48b1887daa9824c186e400269e82bef48feab6
mtszkw <mtszkw@protonmail.com>
2019-07-12 13:48:29 +0200

    Add tracking loop

--- slam.py
+++ slam.py
@@ -33,7 +33,7 @@ if __name__ == "__main__":
     # Read first two frames
     prevFrame = cv2.cvtColor(frameReader.readFrame(0), cv2.COLOR_RGB2GRAY)
     currFrame = cv2.cvtColor(frameReader.readFrame(1), cv2.COLOR_RGB2GRAY)
-    
+
     # Feature detection on the 1st frame
     fast = cv2.FastFeatureDetector_create(threshold=50, nonmaxSuppression=True)
     prevPts = cv2.KeyPoint_convert(fast.detect(prevFrame))
@@ -41,7 +41,7 @@ if __name__ == "__main__":
 
     # Feature tracking on the 2nd frame
     currPts, status, _ = cv2.calcOpticalFlowPyrLK(prevFrame, currFrame, prevPts, None)
-    print ("Tracked features in the second frame, invalid features: {}".format(np.count_nonzero(status == 0)))
+    print ("Tracked {} features in the second frame, invalid features: {}".format(len(currPts), np.count_nonzero(status == 0)))
 
     # Filter out keypoints for which status is zero or coordinates are outside the image
     # ...
@@ -53,18 +53,39 @@ if __name__ == "__main__":
     cy = 0.499021000000000
     focal = (fx + fy) / 2
     pp = (cx, cy)
-    E, mask = cv2.findEssentialMat(prevPts, currPts, focal, pp, cv2.RANSAC, 0.99, 1.0) #, mask?)
+    K = np.array([ [fx, 0, cx], [0, fy, cy], [0, 0, 1] ])
+    
+    E, mask = cv2.findEssentialMat(prevPts, currPts, focal, pp, cv2.RANSAC, 0.99, 1.0)
+    print("Constructed camera matrix {}:\n{}".format(K.shape, K))
     print("Essential matrix {} calculated:\n{}".format(E.shape, E))
-    # R, t = cv2.recoverPose(E, currPts, prevPts, focal, pp, mask)
 
-    # for frameIdx in range(2, frameReader.getFramesCount()):
-    #     frame = frameReader.readFrame(frameIdx)
+    retval, R, t, mask = cv2.recoverPose(E, currPts, prevPts, K)
+    print("Computed rotation matrix {}:\n{}".format(R.shape, R))
+    print("Computed translation matrix {}:\n{}".format(t.shape, t))
+    
+    prevFrame = currFrame
+    prevPts = currPts
 
-    #     # keypts = fast.detect(frame, None)
-    #     frameKeypts = cv2.drawKeypoints(frame, keypts, None, color=(255,0,0))
-    #     cv2.imshow("Frame with keypoints", frameKeypts)
+    # Process next frames
+    for frameIdx in range(2, frameReader.getFramesCount()):
+        currFrame = cv2.cvtColor(frameReader.readFrame(frameIdx), cv2.COLOR_RGB2GRAY)
+        currPts, status, _ = cv2.calcOpticalFlowPyrLK(prevFrame, currFrame, prevPts, None)
+        print("Tracked {} features in frame #{}, valid features: {}".format(len(currPts), frameIdx, np.count_nonzero(status)))
 
-    #     if cv2.waitKey(1) == ord('q'):
-    #         break
+        # invalidIndices = [i for i, elem in enumerate(status) if elem == 0]
+        # print("Invalid indices from tracking in frame #{}: {}".format(frameIdx, invalidIndices))
+        prevPts_new = np.delete(prevPts, status)
+        currPts_new = np.delete(currPts, status)
+        print(currPts.shape)
+        print("Total number of features after tracking in frame #{} = {}".format(frameIdx, len(currPts_new)))
+
+        # frameKeypts = cv2.drawKeypoints(currFrame, cv2.KeyPoint_convert(currPts), None, color=(255,0,0))
+        # cv2.imshow("Frame with keypoints", currFrame)
+
+        # if cv2.waitKey(1) == ord('q'):
+            # break
+
+        prevFrame = currFrame
+        prevPts = currPts
 
     # cv2.destroyAllWindows()
