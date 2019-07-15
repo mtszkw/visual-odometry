8a3feccd91eadfb3b34816cdc22348702fcf8dfe
mtszkw <mtszkw@protonmail.com>
2019-07-15 20:08:06 +0200

    Add FeatureTracker class

---
+++ FeatureTracker.py
@@ -0,0 +1,31 @@
+import cv2
+import numpy as np
+
+class FeatureTracker:        
+    # Finds indices of bad features (with status=0 or position outside the frame).
+    # @param features Detected (or tracked) features.
+    # @param frame Image matrix.
+    # @param status Vector of status flags returned by optical flow.
+    # @returns Vector containing indices that should be filtered out.
+    def calcWrongFeatureIndices(self, features, frame, status):
+        status_ = status.copy()
+        for idx, pt in enumerate(features):
+            if pt[0] < 0 or pt[1] < 0 or pt[0] > frame.shape[1] or pt[1] > frame.shape[0]:
+                status_[idx] = 0
+        wrongIndices = np.where(status_ == 0)[0]
+        return wrongIndices
+
+    # Tracks features using Lucas-Kanade optical flow and filters out bad features.
+    # @param prevFrame Previous image.
+    # @param currFrame Current (next) image.
+    # @param prevPts Features detected on previous frame.
+    # @returns Features from previous and current frame (tracked), both filtered.
+    def trackFeatures(self, prevFrame, currFrame, prevPts):
+        # Feature tracking on the 2nd frame
+        currPts, status, _ = cv2.calcOpticalFlowPyrLK(prevFrame, currFrame, prevPts, None)
+
+        # Filter out features that were not tracked (status=0) or are outside the image
+        wrongIndices = self.calcWrongFeatureIndices(currPts, currFrame, status)
+        prevPts = np.delete(prevPts, wrongIndices, axis=0)
+        currPts = np.delete(currPts, wrongIndices, axis=0)
+        return prevPts, currPts
--- slam.py
+++ slam.py
@@ -2,37 +2,10 @@ import cv2
 import numpy as np
 
 from FrameReader import FrameReader
-
-# Finds indices of bad features (with status=0 or position outside the frame).
-# @param features Detected (or tracked) features.
-# @param frame Image matrix.
-# @param status Vector of status flags returned by optical flow.
-# @returns Vector containing indices that should be filtered out.
-def calcWrongFeatureIndices(features, frame, status):
-    status_ = status.copy()
-    for idx, pt in enumerate(features):
-        if pt[0] < 0 or pt[1] < 0 or pt[0] > frame.shape[1] or pt[1] > frame.shape[0]:
-            status_[idx] = 0
-    wrongIndices = np.where(status_ == 0)[0]
-    return wrongIndices
-
-# Tracks features using Lucas-Kanade optical flow and filters out bad features.
-# @param prevFrame Previous image.
-# @param currFrame Current (next) image.
-# @param prevPts Features detected on previous frame.
-# @returns Features from previous and current frame (tracked), both filtered.
-def trackFeatures(prevFrame, currFrame, prevPts):
-    # Feature tracking on the 2nd frame
-    currPts, status, _ = cv2.calcOpticalFlowPyrLK(prevFrame, currFrame, prevPts, None)
-
-    # Filter out features that were not tracked (status=0) or are outside the image
-    wrongIndices = calcWrongFeatureIndices(currPts, currFrame, status)
-    prevPts = np.delete(prevPts, wrongIndices, axis=0)
-    currPts = np.delete(currPts, wrongIndices, axis=0)
-    return prevPts, currPts
-
+from FeatureTracker import FeatureTracker
 
 # Calculates camera matrix given focal and principal point information.
+# @returns Camera matrix, focal, principal point.
 def calculateCameraMatrix():
     fx = 0.349153000000000
     fy = 0.436593000000000
@@ -45,6 +18,10 @@ def calculateCameraMatrix():
     return K, focal, pp
 
 
+# Draws detected and tracked features on a frame (motion vector is drawn as a line).
+# @param frame Frame to be used for drawing (will be converted to RGB).
+# @param prevPts Previous frame keypoints.
+# @param currPts Next frame keypoints.
 def drawFrameFeatures(frame, prevPts, currPts):
     currFrameRGB = cv2.cvtColor(currFrame, cv2.COLOR_GRAY2RGB)
     for i in range(len(currPts)-1):
@@ -57,16 +34,17 @@ def drawFrameFeatures(frame, prevPts, currPts):
 
 if __name__ == "__main__":
     frameReader = FrameReader("videos/sequence_11/images/")
-    fast = cv2.FastFeatureDetector_create(threshold=50, nonmaxSuppression=True)
-    
+    detector = cv2.FastFeatureDetector_create(threshold=50, nonmaxSuppression=True)
+    tracker = FeatureTracker()
+
     # Feature detection on the 1st frame
     prevFrame = frameReader.readFrame(0)
-    prevPts = cv2.KeyPoint_convert(fast.detect(prevFrame))
+    prevPts = cv2.KeyPoint_convert(detector.detect(prevFrame))
     print("Detected {} features in the first frame".format(len(prevPts)))
 
     # Feature tracking on the 2nd frame
     currFrame = frameReader.readFrame(1)
-    prevPts, currPts = trackFeatures(prevFrame, currFrame, prevPts)
+    prevPts, currPts = tracker.trackFeatures(prevFrame, currFrame, prevPts)
 
     # Calculate camera parameters and find E (focal and p.p. were taken from camera.txt file)
     K, focal, pp = calculateCameraMatrix()
@@ -87,11 +65,10 @@ if __name__ == "__main__":
     for frameIdx in range(2, frameReader.getFramesCount()):
         # Retrack if too many features were filtered out (less than ... points left)
         if len(prevPts) < 25:
-            prevPts = cv2.KeyPoint_convert(fast.detect(currFrame))
+            prevPts = cv2.KeyPoint_convert(detector.detect(currFrame))
         
-        # Read next frame and track features
         currFrame = frameReader.readFrame(frameIdx)
-        prevPts, currPts = trackFeatures(prevFrame, currFrame, prevPts)
+        prevPts, currPts = tracker.trackFeatures(prevFrame, currFrame, prevPts)
 
         E, mask = cv2.findEssentialMat(prevPts, currPts, focal, pp, cv2.RANSAC, 0.99, 1.0)
         retval, R, T, mask = cv2.recoverPose(E, currPts, prevPts, K)
@@ -103,13 +80,10 @@ if __name__ == "__main__":
         # y = int(finalT[1] + (trajectoryImage.shape[0] / 2))
         # cv2.circle(trajectoryImage, (x, y), radius=3, color=(100, 200, 0))
 
-        # Display current frame with motion vectors and some information (frame id, # of features)
         drawFrameFeatures(currFrame, prevPts, currPts)
-        
         if cv2.waitKey(1) == ord('q'):
             break
 
-        # Swap frames before next iteration
         prevFrame = currFrame
         prevPts = currPts
 
